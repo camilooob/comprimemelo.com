d606901afa3c6b3c2621874251151c045be36dba
camilooob <camilobaq@hotmail.com>
2023-02-23 22:48:28 -0500

    clean code

--- app/database.py
+++ app/database.py
@@ -60,12 +60,6 @@ def get_db():
     password_c = str(dic_secret['password'])
     db_host_compri_c = str(dic_secret_comprimelo['db_host_compri'])
     db_name_c = str(dic_secret_comprimelo['db_name'])
-    db_port_c = int(dic_secret_comprimelo['db_port'])
-    jwt_secret_c = str(dic_secret_comprimelo['jwt_secret'])
-    mail_server_c = str(dic_secret_comprimelo['mail_server'])
-    mail_port_c = int(dic_secret_comprimelo['mail_port'])
-    mail_username_c = str(dic_secret_comprimelo['mail_username'])
-    mail_password_c = str(dic_secret_comprimelo['mail_password'])
     if 'db' not in g:
         g.db=mysql.connector.connect(host=db_host_compri_c,user=username_c,password=password_c,database=db_name_c)
     return g.db
