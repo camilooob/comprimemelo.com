07165a8e07dcfe3c9a1b2b38df5bb6ae85e88b0f
camilooob <camilobaq@hotmail.com>
2023-02-23 20:56:14 -0500

    fix tables

--- app/database.py
+++ app/database.py
@@ -175,10 +175,10 @@ class Usuarios(db.Model):
     __tablename__ = 'Usuarios'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(30))
-    last_name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
 
 class Authors(db.Model):  
     __tablename__ = 'Authors'
