858f62a7bb6eaccbf7459968f35e90ab7ef5bb3d
camilooob <camilobaq@hotmail.com>
2023-02-23 19:59:24 -0500

    db

--- app/database.py
+++ app/database.py
@@ -175,7 +175,7 @@ class Usuarios(db.Model):
     __tablename__ = 'Usuarios'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
+    first_name = db.Column(db.String(30))
     last_name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
