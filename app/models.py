458557649e63f4c89450becea05d26c30abd3955
camilooob <camilobaq@hotmail.com>
2023-02-23 23:22:11 -0500

    db

--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ class UserModel(UserMixin):
     def __init__(self, user_data):
         self.id = user_data.username
         self.password = user_data.password
-        self.avatar = user_data.avatar
 
     @staticmethod
     def get(username):
