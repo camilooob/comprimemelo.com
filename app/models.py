9affa250e5d68d8105d1399344da096f0f820bcd
franklinpinto <f.pintoc@uniandes.edu.co>
2023-02-24 20:32:24 -0500

    se modifica print

--- app/models.py
+++ app/models.py
@@ -10,7 +10,5 @@ class UserModel(UserMixin):
     @staticmethod
     def get(email):
         """ Método para obtener el usuario a partir del username. """
-        print(f' get en load_user :: {email}')
         user_data = get_user_by_username(email)
-        print(f' user_data :: {user_data}')
         return UserModel(user_data)
