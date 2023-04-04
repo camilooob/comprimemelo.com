b0e71663d31f0ba2ae7dfa6e87a07b57206d360d
Camilo Baquero <camilobaq@hotmail.com>
2023-04-03 19:44:17 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -61,8 +61,7 @@ def get_db():
     db_host_compri_c = str(dic_secret_comprimelo['db_host_compri'])
     db_name_c = str(dic_secret_comprimelo['db_name'])
     if 'db' not in g:
-        g.db=mysql.connector.connect(host=34.31.69.88,user=root,password=O|)d<k(lP|SK{duY,database= mysql-comprimemelo
-)
+        g.db=mysql.connector.connect()
     return g.db
 
 # Function to close an existing database connection.
